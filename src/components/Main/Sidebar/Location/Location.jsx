343fde92bfc5d525f0a1a0fa0a450b16a87dd055
Vladislav <cheremisin0610@mail.ru>
2021-11-22 21:54:01 +0300

    add sidebar block

--- src/components/Main/Sidebar/Location/Location.jsx
+++ src/components/Main/Sidebar/Location/Location.jsx
@@ -2,7 +2,63 @@ import React from 'react';
 import styles from './Location.module.css';
 
 const Location = () => {
-  return <div>Location</div>;
+  return (
+    <div className={styles.location}>
+      <div className={styles.add}>
+        <div className={styles.plus}>
+          <span>+</span>
+        </div>
+        <div className={styles.titleWrapper}>
+          <div className={styles.title}>Add Location</div>
+          <p className={styles.text}>
+            Did you know you can add favourite locations?
+          </p>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        <li>
+          <img
+            className={styles.arrow}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplIN8Z-vuJrxbqFOxJUNe_CmWjVlaF4B2BQ&usqp=CAU"
+            alt="arrow"
+          />
+          <a href="#">Moscow</a>
+        </li>
+        <li>
+          <img
+            className={styles.arrow}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplIN8Z-vuJrxbqFOxJUNe_CmWjVlaF4B2BQ&usqp=CAU"
+            alt="arrow"
+          />
+          <a href="#">Kolomna</a>
+        </li>
+        <li>
+          <img
+            className={styles.arrow}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplIN8Z-vuJrxbqFOxJUNe_CmWjVlaF4B2BQ&usqp=CAU"
+            alt="arrow"
+          />
+          <a href="#">Kasimov</a>
+        </li>
+        <li>
+          <img
+            className={styles.arrow}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplIN8Z-vuJrxbqFOxJUNe_CmWjVlaF4B2BQ&usqp=CAU"
+            alt="arrow"
+          />
+          <a href="#">Ryazani</a>
+        </li>
+        <li>
+          <img
+            className={styles.arrow}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplIN8Z-vuJrxbqFOxJUNe_CmWjVlaF4B2BQ&usqp=CAU"
+            alt="arrow"
+          />
+          <a href="#">Domodedovo</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Location;
