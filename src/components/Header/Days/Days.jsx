2f2b306418b5f7616a556938285587cac61ced8d
Vladislav <cheremisin0610@mail.ru>
2022-04-18 16:59:00 +0300

    add Select item

--- src/components/Header/Days/Days.jsx
+++ src/components/Header/Days/Days.jsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Days.module.css";
 import { NavLink } from "react-router-dom";
 
-const Days = () => (
-  <ul className={styles.days}>
-    <li>
-      <NavLink to="/">Yesterday</NavLink>
-    </li>
-    <li>
-      <NavLink to="/" className={styles.active}>
-        Today
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Tomorrow</NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Week</NavLink>
-    </li>
-    <li>
-      <NavLink to="/">Month</NavLink>
-    </li>
-  </ul>
-);
+const Days = (props) => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+  return (
+    <ul className={styles.days}>
+      {props.days.map((day, index) => {
+        return (
+          <li
+            onClick={() => onSelectItem(index)}
+            className={activeItem === index ? styles.active : ""}
+            key={`${day} ${index}`}
+          >
+            <NavLink to="/">{day}</NavLink>
+          </li>
+        );
+      })}
+    </ul>
+    // <ul className={styles.days}>
+    //   <li>
+    //     <NavLink to="/">Yesterday</NavLink>
+    //   </li>
+    //   <li>
+    //     <NavLink to="/" className={styles.active}>
+    //       Today
+    //     </NavLink>
+    //   </li>
+    //   <li>
+    //     <NavLink to="/">Tomorrow</NavLink>
+    //   </li>
+    //   <li>
+    //     <NavLink to="/">Week</NavLink>
+    //   </li>
+    //   <li>
+    //     <NavLink to="/">Month</NavLink>
+    //   </li>
+    // </ul>
+  );
+};
 
 export default Days;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = (props) => (
     <div className="container">
       <div className={styles.wrapper}>
         <Logo />
-        <Days />
+        <Days days={["Yesterday", "Today", "Tomorrow", "Week", "Month"]} />
         {props.isAuth ? (
           <div className={styles.login}>
             {props.login}{" "}
